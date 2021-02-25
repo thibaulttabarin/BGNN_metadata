368c5cdd1ee933b947957c77b62fcca6a8da1a4a
Joel Pepper <pepper.54@osu.edu>
2021-02-25 01:21:10 -0500

    Fixed flood filling corner issue

--- gen_metadata.py
+++ gen_metadata.py
@@ -44,6 +44,7 @@ def gen_metadata(file_path):
     cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.3
     predictor = DefaultPredictor(cfg)
     im = cv2.imread(file_path)
+    im_gray = cv2.imread(file_path, cv2.IMREAD_GRAYSCALE)
     metadata = Metadata(evaluator_type='coco', image_root='.',
             json_file='',
             name='metadata',
@@ -108,9 +109,19 @@ def gen_metadata(file_path):
             results['fish_count'] = len(insts[(insts.pred_classes==0).
                 logical_and(insts.scores > 0.3)])
 
-            bbox, mask = gen_mask(
-                    fish.pred_boxes.tensor.cpu().numpy().astype('float64')[i],
-                    file_path, file_name)
+            bbox_d = [round(x) for x in curr_fish.pred_boxes.tensor.cpu().
+                    numpy().astype('float64')[0]]
+            im_crop = im_gray[bbox_d[1]:bbox_d[3],bbox_d[0]:bbox_d[2]]
+            f_bbox_crop = curr_fish.pred_masks[0].cpu().numpy()\
+                    [bbox_d[1]:bbox_d[3],bbox_d[0]:bbox_d[2]]
+            fground = im_crop.reshape(-1)[f_bbox_crop.reshape(-1)]
+            bground = im_crop.reshape(-1)[np.invert(f_bbox_crop.reshape(-1))]
+            sign = -1 if np.mean(bground) > np.mean(fground) else 1
+            #print(np.std(fground))
+            #print(np.std(bground))
+            #print(len(curr_fish.pred_masks[0].cpu().numpy().reshape(-1)))
+            bbox, mask = gen_mask(bbox_d, file_path, file_name,
+                    val=np.mean(bground) + sign * np.std(bground) * 2)
             results['fish'][i]['bbox'] = list(bbox)
             #results['mask'] = mask.astype('uint8').tolist()
 
@@ -173,7 +184,7 @@ def calc_scale(two, three):
     #print(f'Pixels/cm: {scale}')
     return scale
 
-def gen_mask(bbox, file_path, file_name):
+def gen_mask(bbox, file_path, file_name, val=None):
     l = round(bbox[0])
     r = round(bbox[2])
     t = round(bbox[1])
@@ -189,7 +200,10 @@ def gen_mask(bbox, file_path, file_name):
         arr0 = np.array(im.crop(bbox))
         bb_size = arr0.size
 
-        val = filters.threshold_otsu(arr0) * 1.19
+        if val is None:
+            val = filters.threshold_otsu(arr0) * 1.19
+            #print(val)
+        #exit(0)
         #val = filters.threshold_otsu(arr0) * 0.75
         arr1 = np.where(arr0 < val, 1, 0).astype(np.uint8)
         #arr1 = np.where(arr0 > val, 1, 0).astype(np.uint8)
@@ -205,7 +219,9 @@ def gen_mask(bbox, file_path, file_name):
             temp = np.where(temp == 2, 1, 0)
             percent = np.count_nonzero(temp) / bb_size
             if percent > 0.1:
-                temp = flood_fill(temp, (0, 0), 2)
+                for i in (0,temp.shape[0]-1):
+                    for j in (0,temp.shape[1]-1):
+                        temp = flood_fill(temp, (i, j), 2)
                 arr1 = np.where(temp != 2, 1, 0).astype(np.uint8)
                 break
         arr3 = np.full(shape, 0).astype(np.uint8)
@@ -232,114 +248,6 @@ def gen_mask(bbox, file_path, file_name):
     im2.save(f'images/gen_mask_mask_{file_name}')
     return (bbox, arr3)
 
-def gen_temp3(bbox, name):
-    #print(bbox)
-    print(name)
-    l = round(bbox[0])
-    r = round(bbox[2])
-    t = round(bbox[1])
-    b = round(bbox[3])
-
-    im = Image.open(f'{PREFIX_DIR}{IMAGES_DIR}{name}').convert('L')
-    arr2 = np.array(im)
-    shape = arr2.shape
-    bbox = (l,t,r,b)
-    arr0 = np.array(im.crop(bbox))
-    bb_size = arr0.size
-
-    val = filters.threshold_otsu(arr0) * 1.4
-    #val = filters.threshold_otsu(arr0) * 0.75
-    arr1 = np.where(arr0 < val, 1, 0).astype(np.uint8)
-    #arr1 = np.where(arr0 > val, 1, 0).astype(np.uint8)
-    indicies = list(zip(*np.where(arr1 == 1)))
-    shuffle(indicies)
-    count = 0
-    for ind in indicies:
-        count += 1
-        if count > 10000:
-            print(f'ERROR on flood fill: {name}')
-            return None
-        temp = flood_fill(arr1, ind, 2)
-        temp = np.where(temp == 2, 1, 0)
-        percent = np.count_nonzero(temp) / bb_size
-        #print(percent)
-        if percent > 0.1:
-            temp = flood_fill(temp, (0, 0), 2)
-            arr1 = np.where(temp != 2, 1, 0).astype(np.uint8)
-            break
-    arr3 = np.full(shape, 0).astype(np.uint8)
-    arr3[t:b,l:r] = arr1
-    #arr3 = arr1
-    arr3 = np.where(arr3 == 1, 255, 0).astype(np.uint8)
-    im2 = Image.fromarray(arr3, 'L')
-    im2.save(f'image_output/THIS_{name.split(".")[0]}_pixel_mask.jpg')
-    exit(0)
-
-def gen_coco_dataset2():
-    df = pd.read_csv(f'{PREFIX_DIR}boxes.csv', sep=',')
-    #f = partial(wrapper2, df)
-    f = partial(wrapper3, df)
-    #output = [f(0)]
-    #with Pool() as p:
-        #output = p.map(f, list(range(len(df))))
-        #output = p.map(f, list(range(10)))
-    #return [x for x in output if x is not None]
-    with Pool(1) as p:
-        output = map(f, df['Name'].unique())
-        #output = p.map(f, list(range(len(df))))
-        #output = p.map(f, list(range(1000)))
-    g = [x for x in output if x is not None]
-    print(g[0])
-    return g
-
-def wrapper2(df, i):
-    name = df['Name'][i]
-    l = df['x1'][i]
-    r = df['x2'][i]
-    t = df['y1'][i]
-    b = df['y2'][i]
-    print(f'{i}: {name}')
-
-    try:
-        im = Image.open(f'{PREFIX_DIR}{IMAGES_DIR}{name}').convert('L')
-        arr2 = np.array(im)
-        shape = arr2.shape
-        bbox = (l,t,r,b)
-        arr0 = np.array(im.crop(bbox))
-        bb_size = arr0.size
-
-        val = filters.threshold_otsu(arr0) * 1.4
-        arr1 = np.where(arr0 < val, 1, 0).astype(np.uint8)
-        indicies = list(zip(*np.where(arr1 == 1)))
-        shuffle(indicies)
-        count = 0
-        for ind in indicies:
-            count += 1
-            if count > 10000:
-                print(f'ERROR on flood fill: {name}')
-                return None
-            temp = flood_fill(arr1, ind, 2)
-            temp = np.where(temp == 2, 1, 0)
-            percent = np.count_nonzero(temp) / bb_size
-            #print(percent)
-            if percent > 0.1:
-                temp = flood_fill(temp, (0, 0), 2)
-                arr1 = np.where(temp != 2, 1, 0).astype(np.uint8)
-                break
-        arr3 = np.full(shape, 0).astype(np.uint8)
-        arr3[t:b,l:r] = arr1
-        #im2 = Image.fromarray(arr2, 'L')
-        #im2.save(f"/home/jcp353/out_imgs/{name}")
-        return gen_dict(arr3, name, list(bbox))
-    except FileNotFoundError:
-        return None
-
-def gen_coco_dataset():
-    segments = os.listdir(PREFIX_DIR + LM_DIR)
-    names = [i.split('.')[0] for i in segments]
-    with Pool() as p:
-        return p.map(lambda_wrapper, names)
-
 #https://stackoverflow.com/questions/31400769/bounding-box-of-numpy-array
 def shrink_bbox(mask):
     rows = np.any(mask, axis=1)
@@ -355,17 +263,6 @@ def shrink_bbox(mask):
         return None
 
     return (cmin, rmin, cmax, rmax)
-
-def f(name):
-    #curr_nrrd = PREFIX_DIR + LM_DIR + name + '.nrrd'
-    curr_img = PREFIX_DIR + IMAGES_DIR + name + '.jpg'
-    #data, _ = nrrd.read(curr_nrrd, index_order='C')
-    x = np.array(Image.open(curr_img))
-    for i in range(x.shape[0]):
-        for j in range(x.shape[1]):
-            if not x[i][j].any():
-                x[i][j] = np.array([255,255,255])
-    return x
 
 if __name__ == '__main__':
     gen_metadata(sys.argv[1])
